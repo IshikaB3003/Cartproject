a6463232559878b57b37bfd0580c5ba843fe44e2
IshikaB3003 <ishikabansal3003@gmail.com>
2022-09-15 19:24:53 +0530

    added props to cartItem

--- src/Cart.js
+++ src/Cart.js
@@ -2,12 +2,56 @@ import React from 'react';
 import CartItem from './CartItem';
 
     class Cart extends React.Component{
+        constructor(){
+                    //super used because parent ka constructor also needs to be called first
+                    super();
+                    this.state = {
+                        products: [
+                        {price: 999,
+                        title: 'Phone',
+                        qty: 1,
+                        img: '',
+                        id: 1,
+                    },
+                    {price: 899,
+                        title: 'Mobile Phone',
+                        qty: 1,
+                        img: '',
+                        id: 2,
+                    },
+                    {price: 3999,
+                        title: 'Laptop',
+                        qty: 1,
+                        img: '',
+                        id: 3,
+                    },
+                
+                    ]
+                    }
+        
+                }
     render(){
+        const { products } = this.state;
+        // const arr=[1, 2, 3, 4, 5];
         return(
             <div className="cart">
+                {/* <CartItem />
                 <CartItem />
-                <CartItem />
-                <CartItem />
+                <CartItem /> */}
+                {/* {
+                    arr.map((item) => {
+                        return item + 5
+                    })
+                } */}
+                {/* <CartItem qty={1} price={99} title={"Watch"} img={''}/> */}
+                {products.map((product) => {
+                    return ( 
+                    <CartItem 
+                        product={product} 
+                        key={product.id}
+                    />
+                    )
+                })}
             </div>
         );
     }
--- src/CartItem.js
+++ src/CartItem.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 class CartItem extends React.Component{
-    constructor(){
-            //super used because parent ka constructor also needs to be called first
-            super();
-            this.state = {
-                price: 999,
-                title: 'Phone',
-                qty: 1,
-                img: ''
-            }
+    // constructor(){
+    //         //super used because parent ka constructor also needs to be called first
+    //         super();
+    //         this.state = {
+    //             price: 999,
+    //             title: 'Phone',
+    //             qty: 1,
+    //             img: ''
+    //         }
 
-        }
+    //     }
         
 
         testing(){
@@ -97,7 +97,8 @@ class CartItem extends React.Component{
     }
 
     render(){
-        const {price,title,qty} = this.state;
+        console.log('this.props',this.props);
+        const {price,title,qty} = this.props.product;
         return (
             <div className="cart-item">
                 <div className="left-block">
